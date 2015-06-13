c35c9d3c51a0718313f94e7539a6fb774eeddac6
Aleksandar Milinkovic <amilinko@gmail.com>
2015-06-13 11:36:11 +0200

    Lex and sem file are merged into one.

--- common/sim.py
+++ common/sim.py
@@ -1,7 +1,6 @@
+from nltk.corpus import wordnet as wn
 from math import sqrt
 import sys
-from lex import lex_sim
-from sem import sem_sim
 
 # Vector functions
 def magnitude (vector):
@@ -18,6 +17,32 @@ def dotProduct (vector1, vector2):
     for i in range(len(vector1)):
         result = result + vector1[i]*vector2[i]
     return result
+
+# Lexical similarity
+def lcsubstring_length(a, b):
+    table = {}
+    l = 0
+    for i, ca in enumerate(a, 1):
+        for j, cb in enumerate(b, 1):
+            if ca == cb:
+                table[i, j] = table.get((i - 1, j - 1), 0) + 1
+                if table[i, j] > l:
+                    l = table[i, j]
+    return l
+
+def lex_sim(s1, s2):
+    if type(s1) is not str or type(s2) is not str or len(s1)==0 or len(s2)==0:
+        return 0
+    return float(lcsubstring_length(s1, s2)**2)/(len(s1)*len(s2))
+
+# Semantical similarity
+def sem_sim(a, b):
+    x = wn.synsets(a)
+    y = wn.synsets(b)
+    if len(x) == 0 or  len(y) == 0:
+        return lex_sim(a,b)
+    res = x[0].path_similarity(y[0])
+    return  res if res is not None else 0
 
 # Shallow parsing
 def sim_max(w, st, similarity):
